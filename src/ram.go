d92410828de78e9692c3378f4261df73c195ae50
Arun Prakash <arun.prakash.456789@gmail.com>
2023-11-12 03:37:28 +0530

    first commit

---
+++ src/ram.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/shirou/gopsutil/mem"
+)
+
+//RAM details in host
+type RAM struct {
+	Free  uint64 `json:"free"`
+	Total uint64 `json:"total"`
+	Usage uint64 `json:"usage"`
+}
+
+func CheckRAM() RAM {
+	ramChan := make(chan RAM)
+	go func(c chan RAM) {
+		memory, err := mem.VirtualMemory()
+		if err != nil {
+			fmt.Print(err)
+		}
+		c <- RAM{
+			Free:  memory.Total - memory.Used,
+			Usage: memory.Used,
+			Total: memory.Total,
+		}
+	}(ramChan)
+
+	return <-ramChan
+}
